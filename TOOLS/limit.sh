5748e514415988ff0def7f3b2194e17d53158b5e
kope12 <siiintaaa889@gmail.com>
2025-09-09 10:36:09 +0700

    Update limit.sh

--- TOOLS/limit.sh
+++ TOOLS/limit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPO="https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/"
+REPO="https://raw.githubusercontent.com/kope12/GPus/refs/heads/main/"
 
 wget -q -O /etc/systemd/system/limitvmess.service "${REPO}TOOLS/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}TOOLS/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
