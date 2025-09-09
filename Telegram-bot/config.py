2ddaa0f4a05f4fd4f5302fa194c95ce4237fa3d5
kope12 <siiintaaa889@gmail.com>
2025-09-09 12:29:33 +0700

    Update config.py

--- Telegram-bot/config.py
+++ Telegram-bot/config.py
@@ -1,11 +1,11 @@
 # config.py
 
 # Ganti dengan Token Bot Anda
-BOT_TOKEN = ''
+BOT_TOKEN = '8360956749:AAGd3JvDEoulCTJB4QCddb8Y7e-6gG5pv70'
 
 # Ganti dengan Telegram ID Anda (sebagai Admin Utama)
 # Cari tahu ID Anda dengan mengirim pesan ke @userinfobot
-ADMIN_TELEGRAM_ID = 
+ADMIN_TELEGRAM_ID = 6052963873
 
 # Pengaturan Harga
 SSH_PRICE = 1000  # Contoh harga dalam Rupiah (tanpa titik)
