54776e68031bddc82c050008c16da4f9805cc582
monelmurbe <monelmurbe@gmail.com>
2025-09-10 14:17:29 +0700

    Update config.py

--- Telegram-bot/config.py
+++ Telegram-bot/config.py
@@ -1,11 +1,11 @@
 # config.py
 
 # Ganti dengan Token Bot Anda
-BOT_TOKEN = '8342359951:AAHM4qokrS4TgU2fndZILRZNzrztmagJ4BM'
+BOT_TOKEN = '8360956749:AAGd3JvDEoulCTJB4QCddb8Y7e-6gG5pv70'
 
 # Ganti dengan Telegram ID Anda (sebagai Admin Utama)
 # Cari tahu ID Anda dengan mengirim pesan ke @userinfobot
-ADMIN_TELEGRAM_ID = 7942151245
+ADMIN_TELEGRAM_ID = 6052963873
 
 # Pengaturan Harga
 SSH_PRICE = 1000  # Contoh harga dalam Rupiah (tanpa titik)
